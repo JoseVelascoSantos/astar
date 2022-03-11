aac53bda2304238b747198efd896f57a4a409ad0
José Velasco <79725102+jovelasc@users.noreply.github.com>
2022-03-11 10:08:59 +0100

    Test file

---
+++ test.js
@@ -0,0 +1,58 @@
+const {Astar} = require("./Astar");
+
+const a = new Astar(10, 10, 1);
+
+a.addPoint(0, 0);
+a.addPoint(1, 9);
+a.addPoint(9, 9);
+
+a.setObstacle(0, 1);
+a.setObstacle(1, 1);
+a.setObstacle(1, 0);
+
+//a.setRiskyPoint(0, 5, 100);
+
+a.calculate();
+
+console.log(a.getPaths());
+
+
+// ### CONSOLE PRINT ###
+let line = ' |';
+let y;
+for (y = 0; y < a.getMaxY(); y++) {
+    line += ' ' + y + ' |';
+}
+console.log(line);
+for (let x = 0; x < a.getMaxX(); x++) {
+    line = x + '|';
+    for (y = 0; y < a.getMaxY(); y++) {
+        const point = a.getInPoint({x: x, y: y});
+        switch (point) {
+            case exports.START_POINT:
+                line += ' I |';
+                break;
+
+            case exports.END_POINT:
+                line += ' F |';
+                break;
+
+            case exports.OBSTACLE:
+                line += ' # |';
+                break;
+
+            case exports.INACCESSIBLE:
+                line += ' X |';
+                break;
+
+            case exports.RISKY:
+                line += ' R |';
+                break;
+
+            default:
+                line += '   |';
+                break;
+        }
+    }
+    console.log(line);
+}
